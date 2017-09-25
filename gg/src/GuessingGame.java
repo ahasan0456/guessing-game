a7007b70e9998b0d7656ed9fe70713be5fb65a55
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-09-25 12:59:47 -0400

    Functional guessing game.

---
+++ gg/src/GuessingGame.java
@@ -0,0 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class GuessingGame
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String s=input.next();
+		System.out.println("Nice to meet you, "+s+".");
+		int answer=(int)(Math.random()*10+1);
+		int playerGuess=0;
+		while(playerGuess!=answer)
+		{
+			boolean good=false;
+			while(!good)
+			{
+				try
+				{
+					Scanner thing=new Scanner(System.in);
+					System.out.println("Enter your guess.");
+					playerGuess=thing.nextInt();
+					good=true;
+				}
+				catch(InputMismatchException e)
+				{
+					System.out.println("That was not a number.");
+				}
+			}
+			if(playerGuess!=answer)
+			{
+				System.out.println("That is not the right number. Try again.");
+			}
+		}
+		System.out.println("You guessed it right!");
+	}
+}
